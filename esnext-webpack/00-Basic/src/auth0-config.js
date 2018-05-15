9170c49ca4f53f31d26f9b2efd9f20ddb992ddf6
Nikolaj Ivancic <nik@congral.com>
2018-05-15 18:03:58 -0400

    Update auth0-config.js

--- esnext-webpack/00-Basic/src/auth0-config.js
+++ esnext-webpack/00-Basic/src/auth0-config.js
@@ -1,5 +1,5 @@
 export const AuthConfig = {
   domain: 'aureliatools.auth0.com',
-  clientID: 'RWbqj2vOW54nCeTc6J1oOgwpi7I58w7j',
-  callbackUri: 'http://localhost:8080/callback'
+  clientID: 'place your own cliendID here',
+  callbackUri: 'http://localhost:8080'
 }
