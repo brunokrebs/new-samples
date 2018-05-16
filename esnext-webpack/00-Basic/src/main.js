e777853ecf3541ce9056daad8eabcf92af5344f0
Nikolaj Ivancic <nik@congral.com>
2018-05-16 13:03:30 -0400

    nc

---
+++ esnext-webpack/00-Basic/src/main.js
@@ -0,0 +1,28 @@
+import environment from './environment';
+import {PLATFORM} from 'aurelia-pal';
+import 'babel-polyfill';
+import * as Bluebird from 'bluebird';
+
+import '../static/styles.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
+Bluebird.config({ warnings: { wForgottenReturn: false } });
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature(PLATFORM.moduleName('resources/index'))
+    .plugin('aurelia-event-aggregator');
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+  }
+
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+}
